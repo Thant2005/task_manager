addea39950029efcac4d74ea9c028b5c1f0e22a1
LAPTOP-Q0HKJNMT\hp <zawwinthant317@gmail.com>
2026-07-05 00:57:02 +0630

    fix: resolve build

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,5 @@
 import { createRoot } from "react-dom/client";
 import "./index.css";
-
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import Router from "./router/index.tsx";
 import { AuthProvider } from "./contexts/authContext.tsx";
